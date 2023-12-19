3d9803f5adc53f6f107809ec411f771bb573c805
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-19 12:57:06 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,37 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
-  const [input, setInput]= useState('')
-  const [filteredFruits, setFilteredFruits]= useState(fruits)
+  const [input, setInput] = useState('');
+  const [filteredFruits, setFilteredFruits] = useState(fruits);
 
-
-  const setInputValue = (e)=>{
-    setInput(e.target.value)
-    updateList();
-  }
-  const updateList = ()=>{
-    setFilteredFruits(fruits.filter((item)=>{return item.includes(input.toLowerCase())}))
-  }
-
+  const setInputValue = (e) => {
+    setInput(e.target.value);
+  };
   
+  useEffect(()=>{
+    updateList();
+  },[input])
+  const updateList = () => {
+    setFilteredFruits(fruits.filter((item) => item.toLowerCase().includes(input.toLowerCase())));
+  };
 
   return (
     <>
-    <h1>Search item</h1>
-    <div>
+      <h1>Search item</h1>
+      <div>
         {/* Do not remove the main div */}
-        <input type="text"  value={input} onChange={setInputValue}/>
-    </div>
+        <input type="text" value={input} onChange={setInputValue} />
+      </div>
 
-    <ul>
-      { filteredFruits.map((item,index)=>{return <li key={index}>{item}</li>})}
-    </ul>
+      <ul>
+        {filteredFruits.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
